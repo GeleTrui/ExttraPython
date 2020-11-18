0d4bd0c4a8d7ffa5a26ad9557296530cff35f019
GeleTrui <70946651+GeleTrui@users.noreply.github.com>
2020-11-18 14:49:07 +0100

    awww yeeeeeee

---
+++ maak_mappen.py
@@ -0,0 +1,40 @@
+import io
+import os
+
+# Bestand in read-only (r) mode openen (wel zo veilig, we gaan niets overschrijven)
+bestand = open("klasgenoten.txt", "r")
+
+# Een tekst naar het bestand schrijven
+regel1 = bestand.readline()
+print(regel1)
+
+regel2 = bestand.readline()
+regel2 = regel.strip()
+print(regel2)
+
+regel3 = bestand.readline()
+tekst_regel = tekst_regel.strip()
+print(regel3)
+regel4 = bestand.readline()
+tekst_regel = tekst_regel.strip()
+print(regel4)
+regel5 = bestand.readline()
+tekst_regel = tekst_regel.strip()
+print(regel5)
+regel6 = bestand.readline()
+tekst_regel = tekst_regel.strip()
+print(regel6)
+regel7 = bestand.readline()
+tekst_regel = tekst_regel.strip()
+print(regel7)
+regel8 = bestand.readline()
+tekst_regel = tekst_regel.strip()
+print(regel8)
+regel9 = bestand.readline()
+tekst_regel = tekst_regel.strip()
+print(regel9)
+regel10 = bestand.readline()
+tekst_regel = tekst_regel.strip()
+print(regel10)
+
+os.mkdir()
